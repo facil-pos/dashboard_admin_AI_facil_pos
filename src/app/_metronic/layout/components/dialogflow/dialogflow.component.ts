405570d347acbb9b38ad5d5e1a5f42934ea0bc8b
Dannkol <manosalvasierra@gmail.com>
2024-06-22 10:19:58 -0500

    feat: :sparkles: add custom dialogflow

--- src/app/_metronic/layout/components/dialogflow/dialogflow.component.ts
+++ src/app/_metronic/layout/components/dialogflow/dialogflow.component.ts
@@ -9,6 +9,15 @@ export class DialogflowComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
+    // Add event listener to send a welcome message once the Dialogflow Messenger is loaded
+    console.log('Component initialized');
 
+    setTimeout(() => {
+      window.addEventListener('df-messenger-loaded', () => {
+        console.log('Chat iniciado');
+        const chatBox = document.querySelector('df-messenger') as any;
+        chatBox?.renderCustomText('Hola, ¿en qué puedo ayudarte hoy?');
+      });
+    }, 1000);
+  }
 }
